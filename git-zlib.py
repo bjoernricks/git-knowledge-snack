14444ffd1790e649cfb29b879c6a371ccd3b93d1
Björn Ricks <bjoern.ricks@greenbone.net>
2024-03-08 10:39:16 +0100

    Change: Replace NULL bytes with linefeed for better output
    The git objects have a NULL byte after the size. For better output
    in the terminal replace the NULL byte with a line feed.

--- git-zlib.py
+++ git-zlib.py
@@ -7,7 +7,7 @@ from pathlib import Path
 def main() -> None:
     file = Path(sys.argv[1])
     data = file.read_bytes()
-    print(zlib.decompress(data).decode(errors="replace"))
+    print(zlib.decompress(data).decode(errors="replace").replace("\0", "\n"))
 
 
 if __name__ == "__main__":
